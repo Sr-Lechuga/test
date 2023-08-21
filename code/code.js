d029316a7a13c2684d8eacadd09a74ec3adad217
Sr.Lechuga <jonattanlima@gmail.com>
2023-08-20 22:06:55 -0300

    Intersection Observer implementation

--- code/code.js
+++ code/code.js
@@ -1,2 +1,21 @@
 "use stric";
+const cajas = document.querySelectorAll(".caja");
 
+verifyVisibility = (entries) => {
+    // const entry = entries[0];
+    // console.log(entry.isIntersecting);
+
+    // There could be many entries since could be observing multiple elements in the page
+    for(entry of entries){
+        if(entry.isIntersecting) console.log(`Se esta viendo la caja ${entry.target.textContent}`)
+    }
+}
+
+const observer = new IntersectionObserver(verifyVisibility);
+
+// observer.observe(caja03);
+
+for (caja of cajas){
+    //Set the observer, to observe each one of the boxes
+    observer.observe(caja);
+}
