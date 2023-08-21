3cdd1a3c6e79017c14e53041b3bf8c5779f1014c
Sr.Lechuga <jonattanlima@gmail.com>
2023-08-20 21:50:30 -0300

    Intersection observer initial

--- code/code.js
+++ code/code.js
@@ -1,22 +1,2 @@
 "use stric";
 
-const mq = matchMedia("(max-width: 700px)");
-let caja = document.querySelector(".caja");
-
-//Es equivalente a hacer un media query
-// console.log(mq);
-
-//En caso que la dimension del vizualizador concuerde con las propiedades definidas devuelve true
-// console.log(mq.matches);
-
-mq.addEventListener("change", ()=>{
-    console.log("Resolucion cambiada");
-    if(mq.matches){
-        caja.classList.replace("caja","responsive-caja");
-        caja = document.querySelector(".responsive-caja")
-    }
-    else{
-        caja.classList.replace("responsive-caja","caja");
-        caja = document.querySelector(".caja");
-    }
-});
