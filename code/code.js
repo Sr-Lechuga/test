0ca2de3c492638862f71aa95f182aa9919de6bbf
Sr.Lechuga <jonattanlima@gmail.com>
2023-08-20 21:40:09 -0300

    Match media test

--- code/code.js
+++ code/code.js
@@ -1,13 +1,22 @@
 "use stric";
 
-const mq = matchMedia("(max-width: 500px)");
+const mq = matchMedia("(max-width: 700px)");
+let caja = document.querySelector(".caja");
 
 //Es equivalente a hacer un media query
-console.log(mq);
+// console.log(mq);
 
 //En caso que la dimension del vizualizador concuerde con las propiedades definidas devuelve true
-console.log(mq.matches);
+// console.log(mq.matches);
 
 mq.addEventListener("change", ()=>{
     console.log("Resolucion cambiada");
+    if(mq.matches){
+        caja.classList.replace("caja","responsive-caja");
+        caja = document.querySelector(".responsive-caja")
+    }
+    else{
+        caja.classList.replace("responsive-caja","caja");
+        caja = document.querySelector(".caja");
+    }
 });
